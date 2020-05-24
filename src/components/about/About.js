a10ba23ae06ac9fdd29d1cc6eb949f1fc39827ad
Nick Colavita <nickcolavita@Nicks-MBP.fios-router.home>
2020-05-24 11:44:57 -0400

    adding about me page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -5,39 +5,57 @@ import './About.css';
 const About = () => {
     return (
         <div className="about-container">
-            <div className="headline">
-                <h4>Hi! As you can tell, I am</h4>
-                <h1>Nick Colavita</h1>
-                <h4>Front End Developer</h4>
-                <p>
-                    Ability to grow, adapt, learn, and build, I have been able
-                    to focus on communication through the web. Driven by
-                    curiosity &amp; willingness to explore, there has been
-                    little that hasn't fascinated me.
-                </p>
+            <div className="left">
+                <div className="headline">
+                    <h4>Hi! As you can tell, I am</h4>
+                    <h1>Nick Colavita</h1>
+                    <ul>
+                        <li>
+                            <h4>Front End Developer</h4>
+                        </li>
+                        <li>
+                            <h4>Storyteller</h4>
+                        </li>
+                        <li>
+                            <h4>Data Visualization Enthusiast</h4>
+                        </li>
+                    </ul>
+                    <p>
+                        Ability to grow, adapt, learn, and build, I have focused
+                        on communication through the web. Driven by curiosity
+                        &amp; willingness to explore, there has been little that
+                        hasn't fascinated me.
+                    </p>
+                </div>
+                <div className="skills">
+                    <h1>Skills</h1>
+                    <div className="skill">
+                        <span className="html5" style={skill}></span>
+                        <h4>HTML5</h4>
+                    </div>
+                    <div className="skill">
+                        <span className="css3" style={skill}></span>
+                        <h4>CSS3</h4>
+                    </div>
+                    <div className="skill">
+                        <span className="js" style={skill}></span>
+                        <h4>JS</h4>
+                    </div>
+                    <div className="skill">
+                        <span className="react" style={skill}></span>
+                        <h4>React</h4>
+                    </div>
+                    <div className="skill">
+                        <span className="dataviz" style={skill}></span>
+                        <h4>Data Visualization</h4>
+                    </div>
+                </div>
             </div>
-            <div className="skills">
-                <h1>Skills</h1>
-                <div className="skill">
-                    <span className="html5" style={skill}></span>
-                    <h4>HTML5</h4>
-                </div>
-                <div className="skill">
-                    <span className="css3" style={skill}></span>
-                    <h4>CSS3</h4>
-                </div>
-                <div className="skill">
-                    <span className="js" style={skill}></span>
-                    <h4>JS</h4>
-                </div>
-                <div className="skill">
-                    <span className="react" style={skill}></span>
-                    <h4>React</h4>
-                </div>
-                <div className="skill">
-                    <span className="dataviz" style={skill}></span>
-                    <h4>Data Visualization</h4>
-                </div>
+            <div className="right">
+                <div className="small-square"></div>
+                <div className="small-square2"></div>
+                <div className="small-square3"></div>
+                <div className="large-square"></div>
             </div>
         </div>
     );
